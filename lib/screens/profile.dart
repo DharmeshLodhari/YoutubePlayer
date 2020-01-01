cee178331598e834afeffcbac3f088cb9458a8af
abi <abiola.rasheed@novastonemedia.com>
2020-01-01 21:31:45 +0000

    Fixed profile page textfield padding

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -21,69 +21,67 @@ class _ProfileState extends State<Profile> {
         backgroundColor: Colors.green,
       ),
       body: Center(
-        child: SingleChildScrollView(
-          child: Container(
-            height: 600,
-            color: Colors.white,
-            padding: EdgeInsets.all(30),
-            child: Center(
-              child: Column(
-                children: <Widget>[
-                  SizedBox(height: 10),
-                  Center(
-                    child: Card(
-                      semanticContainer: true,
-                      elevation: 4.0,
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Container(
-                            padding: EdgeInsets.all(40),
-                            child: Image.network(
-                              userBloc.user.qrCode,
-                              colorBlendMode: BlendMode.darken,
-                              fit: BoxFit.fitWidth,
-                              filterQuality: FilterQuality.high,
-                            ),
+        child: Container(
+          //height: 600,
+          color: Colors.green,
+          padding: EdgeInsets.all(30),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 10),
+                Center(
+                  child: Card(
+                    semanticContainer: true,
+                    elevation: 4.0,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.all(40),
+                          child: Image.network(
+                            userBloc.user.qrCode,
+                            colorBlendMode: BlendMode.darken,
+                            fit: BoxFit.fitWidth,
+                            filterQuality: FilterQuality.high,
                           ),
-                          ButtonBar(
-                            children: <Widget>[
-                              FlatButton(
-                                  onPressed: () {},
-                                  child: Text(userBloc.user.fullName,
-                                      style: TextStyle(
-                                          color: Colors.black, fontSize: 14))),
-                              FlatButton.icon(
-                                  onPressed: () {},
-                                  icon:
-                                      Icon(Icons.settings, color: Colors.black),
-                                  label: Text('Copy Url',
-                                      style: TextStyle(
-                                          color: Colors.black, fontSize: 14))),
-                            ],
-                          ),
-                        ],
-                      ),
+                        ),
+                        ButtonBar(
+                          mainAxisSize: MainAxisSize.max,
+                          alignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            FlatButton(
+                                onPressed: () {},
+                                child: Text(userBloc.user.fullName,
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 14))),
+                            FlatButton.icon(
+                                onPressed: () {},
+                                icon: Icon(Icons.settings, color: Colors.black),
+                                label: Text('Copy Url',
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 14))),
+                          ],
+                        ),
+                      ],
                     ),
                   ),
-                  SizedBox(height: 30),
-                  ButtonTheme(
-                    //elevation: 4,
-                    //color: Colors.green,
-                    minWidth: double.infinity,
-                    child: MaterialButton(
-                      elevation: 4.0,
-                      onPressed: () {
-                        Navigator.of(context).pushNamed('/send-payment');
-                      },
-                      textColor: Colors.white,
-                      color: Colors.green,
-                      height: 50,
-                      child: Text("Make a Payment"),
-                    ),
-                  )
-                ],
-              ),
+                ),
+                SizedBox(height: 30),
+                ButtonTheme(
+                  //elevation: 4,
+                  minWidth: double.infinity,
+                  child: MaterialButton(
+                    elevation: 4.0,
+                    onPressed: () {
+                      Navigator.of(context).pushNamed('/send-payment');
+                    },
+                    textColor: Colors.black,
+                    color: Colors.white,
+                    height: 50,
+                    child: Text("Make a Payment"),
+                  ),
+                )
+              ],
             ),
           ),
         ),
