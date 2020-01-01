562a1f75e0554253946bdaa018f8d293f8e245c3
abi <abiola.rasheed@novastonemedia.com>
2020-01-01 04:24:14 +0000

    Added Jwt table on db creation

--- lib/data/database_helper.dart
+++ lib/data/database_helper.dart
@@ -6,7 +6,6 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 
-
 class DatabaseHelper {
   static final DatabaseHelper _instance = new DatabaseHelper.internal();
   factory DatabaseHelper() => _instance;
@@ -14,8 +13,7 @@ class DatabaseHelper {
   static Database _db;
 
   Future<Database> get db async {
-    if(_db != null)
-      return _db;
+    if (_db != null) return _db;
     _db = await initDb();
     return _db;
   }
@@ -29,12 +27,14 @@ class DatabaseHelper {
     return theDb;
   }
 
-
   void _onCreate(Database db, int version) async {
     // When creating the db, create the table
     await db.execute(
         "CREATE TABLE User(uuid TEXT PRIMARY KEY, fullName TEXT, userName TEXT, "
-            "phoneNumber TEXT, password TEXT, avatar TEXT, qrCode TEXT, url TEXT)");
+        "phoneNumber TEXT, password TEXT, avatar TEXT, qrCode TEXT, url TEXT)");
+
+    await db.execute(
+        "CREATE TABLE Jwt(access TEXT PRIMARY KEY, refresh TEXT, expiration TEXT)");
   }
 
   Future<int> saveUser(User user) async {
@@ -49,14 +49,12 @@ class DatabaseHelper {
     return res;
   }
 
-
   Future close() async => _db.close();
-
 
   Future<bool> isLoggedIn() async {
     var dbClient = await db;
     var res = await dbClient.query("User");
-    return res.length > 0? true: false;
+    return res.length > 0 ? true : false;
   }
 
   Future<User> getUser() async {
@@ -65,10 +63,16 @@ class DatabaseHelper {
     var res = await dbClient.query("User");
     List<User> users = [];
 
-    for(var obj in res){
-      var u = User(uuid: obj["uuid"], url: obj["url"], phoneNumber: obj["phoneNumber"],
-          fullName: obj["fullName"], userName: obj["userName"], avatar: obj["avatar"],
-          qrCode: obj["qrCode"], password: obj["password"]);
+    for (var obj in res) {
+      var u = User(
+          uuid: obj["uuid"],
+          url: obj["url"],
+          phoneNumber: obj["phoneNumber"],
+          fullName: obj["fullName"],
+          userName: obj["userName"],
+          avatar: obj["avatar"],
+          qrCode: obj["qrCode"],
+          password: obj["password"]);
       users.add(u);
     }
     return users[0];
