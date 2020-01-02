1815f669cf880362a61b2b6c093bb9626d1039ef
abi <abiola.rasheed@novastonemedia.com>
2020-01-02 20:29:22 +0000

    Updated QRCodeView page with back botton

--- lib/screens/forms/payment.dart
+++ lib/screens/forms/payment.dart
@@ -1,178 +1,204 @@
-import 'dart:convert';
-
-import 'package:PayBay/models/user.dart';
-import 'package:PayBay/screens/commons.dart';
+import 'package:PayBay/data/state_notifier.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
 
 class SendPayment extends StatefulWidget {
+  // Declare a field that holds the userData.
   @override
   _SendPaymentState createState() => _SendPaymentState();
 }
 
 class _SendPaymentState extends State<SendPayment> {
-  Future<Payee> _getPayee() async {
-    final String payeeURL =
-        "https://api.mockaroo.com/api/3bbfbdf0?count=1&key=b81ba250";
-    var response = await http.get(payeeURL);
-
-    if (response.statusCode == 200) {
-      var jsonData = json.decode(response.body);
-
-      Payee _payee = Payee(
-          uuid: jsonData['uuid'],
-          url: jsonData['url'],
-          fullName: jsonData['fullName'],
-          userName: jsonData['userName'],
-          avatar: jsonData['avatar'],
-          qrCode:
-              'https://cdn.britannica.com/s:700x500/17/155017-050-9AC96FC8/Example-QR-code.jpg');
-      return _payee;
-    } else {
-      throw "Can't get https.";
-    }
-  }
+  final _formKey = GlobalKey<FormState>();
+  int _currentIndex = 2;
 
   @override
   Widget build(BuildContext context) {
+    final PayeeBloc payeeBloc = Provider.of<PayeeBloc>(context);
+    final UserBloc userBloc = Provider.of<UserBloc>(context);
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: Center(child: Text("Send a Payment")),
-        backgroundColor: Colors.green,
-      ),
-      body: FutureBuilder(
-        future: _getPayee(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.data == null) {
-            return Container(
-              child: Text('missing'),
-            );
-          } else {
-            return Center(
-              child: SingleChildScrollView(
-                child: Container(
-                  height: 600,
-                  color: Colors.white,
-                  padding: EdgeInsets.all(40),
-                  child: Center(
-                    child: Column(
-                      children: <Widget>[
-                        SizedBox(height: 10),
-                        Center(
-                          child: Card(
-                            semanticContainer: true,
-                            elevation: 4.0,
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                SizedBox(
-                                  height: 20,
-                                ),
-                                ListTile(
-                                  title: Text(
-                                    snapshot.data.fullName,
-                                    style: TextStyle(
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 15),
-                                  ),
-                                  subtitle: Text(snapshot.data.userName),
-                                  leading: Image.network(
-                                    snapshot.data.avatar,
-                                    colorBlendMode: BlendMode.darken,
-                                    fit: BoxFit.fitWidth,
-                                    filterQuality: FilterQuality.high,
-                                  ),
-                                  trailing: Image.network(
-                                    snapshot.data.qrCode,
-                                    colorBlendMode: BlendMode.darken,
-                                    fit: BoxFit.fitWidth,
-                                    filterQuality: FilterQuality.high,
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 20,
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.all(20.0),
-                                  child: TextField(
-                                    autofocus: false,
-                                    obscureText: false,
-                                    keyboardType: TextInputType.number,
-                                    decoration: InputDecoration(
-                                        prefixText: '#',
-                                        prefixStyle: TextStyle(
-                                            color: Colors.green,
-                                            backgroundColor: Colors.grey[400],
-                                            fontSize: 20,
-                                            letterSpacing: 5),
-                                        //labelText: "Enter Amount",
-                                        hintText: "Enter Amount",
-                                        labelStyle: TextStyle(
-                                          color: Colors.black,
-                                          fontSize: 16,
-                                        ),
-                                        border: OutlineInputBorder(
-                                            borderRadius: BorderRadius.all(
-                                                Radius.circular(4)),
-                                            borderSide: BorderSide(
-                                                width: 1,
-                                                color: Colors.green,
-                                                style: BorderStyle.solid))),
-                                  ),
-                                ),
-                                Padding(
-                                    padding: EdgeInsets.all(20.0),
-                                    child: TextField(
-                                      autofocus: false,
-                                      obscureText: false,
-                                      //keyboardType: TextInputType.phone,
-                                      decoration: InputDecoration(
-                                          labelText: "Reference",
-                                          hintText: "Reference",
-                                          labelStyle: TextStyle(
-                                            color: Colors.black,
-                                            fontSize: 16,
-                                          ),
-                                          border: OutlineInputBorder(
-                                              borderRadius: BorderRadius.all(
-                                                  Radius.circular(4)),
-                                              borderSide: BorderSide(
-                                                  width: 1,
-                                                  color: Colors.green,
-                                                  style: BorderStyle.solid))),
-                                    )),
-                              ],
+          title: Center(child: Text("Send a Payment")),
+          backgroundColor: Colors.green),
+      body: SingleChildScrollView(
+        child: Container(
+          height: 600,
+          color: Colors.white,
+          padding: EdgeInsets.all(40),
+          child: Center(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 10),
+                  Center(
+                    child: Card(
+                      semanticContainer: true,
+                      elevation: 4.0,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          SizedBox(
+                            height: 20,
+                          ),
+                          ListTile(
+                            title: Text(
+                              payeeBloc.payee.fullName,
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15),
+                            ),
+                            subtitle: Text(payeeBloc.payee.userName),
+                            leading: Image.network(
+                              payeeBloc.payee.avatar,
+                              colorBlendMode: BlendMode.darken,
+                              fit: BoxFit.fitWidth,
+                              filterQuality: FilterQuality.high,
+                            ),
+                            trailing: Image.network(
+                              payeeBloc.payee.qrCode,
+                              colorBlendMode: BlendMode.darken,
+                              fit: BoxFit.fitWidth,
+                              filterQuality: FilterQuality.high,
                             ),
                           ),
-                        ),
-                        SizedBox(height: 30),
-                        ButtonTheme(
-                          //elevation: 4,
-                          //color: Colors.green,
-                          minWidth: double.infinity,
-                          child: MaterialButton(
-                            elevation: 4.0,
-                            onPressed: () {
-                              Navigator.of(context).pushNamed('/transactions');
-                            },
-                            textColor: Colors.white,
-                            color: Colors.green,
-                            height: 50,
-                            child: Text("Send Payment"),
+                          SizedBox(
+                            height: 20,
                           ),
-                        )
-                      ],
+                          Padding(
+                            padding: EdgeInsets.all(20.0),
+                            child: TextFormField(
+                              autofocus: true,
+                              obscureText: false,
+                              //keyboardType: TextInputType.number,
+                              keyboardType: TextInputType.number,
+                              inputFormatters: [
+                                WhitelistingTextInputFormatter.digitsOnly
+                              ],
+                              decoration: InputDecoration(
+                                  prefixText: '#',
+                                  prefixStyle: TextStyle(
+                                      color: Colors.green,
+                                      backgroundColor: Colors.grey[400],
+                                      fontSize: 20,
+                                      letterSpacing: 5),
+                                  //labelText: "Enter Amount",
+                                  hintText: "Enter Amount",
+                                  labelStyle: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                  ),
+                                  border: OutlineInputBorder(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(4)),
+                                      borderSide: BorderSide(
+                                          width: 1,
+                                          color: Colors.green,
+                                          style: BorderStyle.solid))),
+                            ),
+                          ),
+                          Padding(
+                              padding: EdgeInsets.all(20.0),
+                              child: TextFormField(
+                                autofocus: false,
+                                decoration: InputDecoration(
+                                    labelText: "Reference",
+                                    hintText: "Reference",
+                                    labelStyle: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16,
+                                    ),
+                                    border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(4)),
+                                        borderSide: BorderSide(
+                                            width: 1,
+                                            color: Colors.green,
+                                            style: BorderStyle.solid))),
+                              )),
+                        ],
+                      ),
                     ),
                   ),
-                ),
+                  SizedBox(height: 30),
+                  ButtonTheme(
+                    //elevation: 4,
+                    //color: Colors.green,
+                    minWidth: double.infinity,
+                    child: MaterialButton(
+                      elevation: 4.0,
+                      onPressed: () {
+                        Navigator.of(context).pushNamed('/transactions');
+                      },
+                      textColor: Colors.white,
+                      color: Colors.green,
+                      height: 50,
+                      child: Text("Send Payment"),
+                    ),
+                  )
+                ],
               ),
-            );
+            ),
+          ),
+        ),
+      ),
+
+// TODO: Find a better way to do this without duplication
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+
+          String path = _currentIndex.toString();
+
+          switch (path) {
+            case '0':
+              return Navigator.of(context).pushNamed('/profile');
+            case '1':
+              return Navigator.of(context).pushNamed('/accounts');
+            case '2':
+              return Navigator.of(context).pushNamed('/transactions');
+            case '3':
+              return Navigator.of(context).pushNamed('/settings');
+            default:
+              // If there is no such named route in the switch statement, e.g. /third
+              return Navigator.of(context).pushNamed('/profile');
           }
         },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: Colors.grey[400],
+            ),
+            title: Text('Home',
+                style: TextStyle(color: Colors.grey[400], fontSize: 12)),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group, color: Colors.grey[400]),
+            title: Text('Accounts',
+                style: TextStyle(color: Colors.grey[400], fontSize: 12)),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart, color: Colors.grey[400]),
+            title: Text('Transactions',
+                style: TextStyle(color: Colors.grey[400], fontSize: 12)),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings, color: Colors.grey[400]),
+            title: Text('Settings',
+                style: TextStyle(color: Colors.grey[400], fontSize: 12)),
+          ),
+        ],
       ),
-      bottomNavigationBar: bottomNavigationBar,
     );
+    //
   }
 }
--- lib/screens/scan_qr_code.dart
+++ lib/screens/scan_qr_code.dart
@@ -24,7 +24,15 @@ class _QRCodeViewState extends State<QRCodeView> {
 
   @override
   Widget build(BuildContext context) {
+    final PayeeBloc payeeBloc = Provider.of<PayeeBloc>(context);
+    final UserBloc userBloc = Provider.of<UserBloc>(context);
+
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        actions: <Widget>[],
+      ),
       backgroundColor: Colors.transparent,
       body: Column(
         children: <Widget>[
@@ -48,9 +56,34 @@ class _QRCodeViewState extends State<QRCodeView> {
               children: <Widget>[
                 //Text("This is the result of scan: $qrText"),
                 RaisedButton(
-                  onPressed: () {
+                  onPressed: () async {
                     if (controller != null) {
                       controller.flipCamera();
+                      String url = ums + "/api/v1/customer/alex.rasheed.2";
+
+                      if (url.startsWith(ums)) {
+                        Map<String, dynamic> jsonResponse = {
+                          'full_name': 'Alex Rasheed',
+                          'uuid': '',
+                          'url':
+                              'http://192.168.1.5:8080/media/customer/avatar/me_9c9uSF2.jpeg',
+                          'username': 'alex.rasheed.2',
+                          'avatar':
+                              'http://192.168.1.5:8080/media/customer/avatar/me_rWdkxLb.jpeg',
+                          'qr_code':
+                              'http://192.168.1.5:8080/media/customer/qr-code/2b439ab4d0b343aab8360d6e38f6e83a.png'
+                        };
+                        Payee _payee = Payee(
+                          uuid: jsonResponse['uuid'],
+                          url: jsonResponse['url'],
+                          fullName: jsonResponse['full_name'],
+                          userName: jsonResponse['username'],
+                          avatar: jsonResponse['avatar'],
+                          qrCode: jsonResponse['qr_code'],
+                        );
+                        payeeBloc.payee = _payee;
+                        Navigator.of(context).pushNamed('/send-payment');
+                      }
                     }
                   },
                   child: Text('Flip', style: TextStyle(fontSize: 20)),
@@ -71,30 +104,30 @@ class _QRCodeViewState extends State<QRCodeView> {
   }
 
   void _onQRViewCreated(QRViewController controller) {
-    final PayeeBloc payeeBloc = Provider.of<PayeeBloc>(context);
+    //final PayeeBloc payeeBloc = Provider.of<PayeeBloc>(context);
     this.controller = controller;
     controller.scannedDataStream.listen((scanData) async {
       // if we get a text that belongs to us then we process it
-      if (scanData.startsWith(ums)) {
-        var response = await http.get(scanData);
-        if (response.statusCode == 200) {
-          var jsonResponse = json.decode(response.body);
-          if (jsonResponse != null) {
-            Payee _payee = Payee(
-              uuid: '', //jsonResponse['uuid'],
-              url: "", //jsonResponse['url'],
-              fullName: jsonResponse['full_name'],
-              userName: jsonResponse['username'],
-              avatar: jsonResponse['avatar']
-                  .replaceAll("http://127.0.0.1:8080", ums),
-              qrCode: jsonResponse['qrcode']
-                  .replaceAll("http://127.0.0.1:8080", ums),
-            );
-            payeeBloc.payee = _payee;
-            Navigator.of(context).pushNamed('/send-payment');
-          }
-        }
-      }
+//      if (scanData.startsWith(ums)) {
+//        var response = await http.get(scanData);
+//        if (response.statusCode == 200) {
+//          var jsonResponse = json.decode(response.body);
+//          if (jsonResponse != null) {
+//            Payee _payee = Payee(
+//              uuid: '', //jsonResponse['uuid'],
+//              url: "", //jsonResponse['url'],
+//              fullName: jsonResponse['full_name'],
+//              userName: jsonResponse['username'],
+//              avatar: jsonResponse['avatar']
+//                  .replaceAll("http://127.0.0.1:8080", ums),
+//              qrCode: jsonResponse['qrcode']
+//                  .replaceAll("http://127.0.0.1:8080", ums),
+//            );
+//            payeeBloc.payee = _payee;
+//            Navigator.of(context).pushNamed('/send-payment');
+//          }
+//        }
+//      }
     });
   }
 }
