f09d3a4d147245bdb535e6bda784603155260c49
abi <abiola.rasheed@novastonemedia.com>
2020-02-04 22:54:17 +0000

    Cleaned Up add account page

--- lib/screens/forms/add_bank_account.dart
+++ lib/screens/forms/add_bank_account.dart
@@ -10,7 +10,6 @@ class AddAccount extends StatefulWidget {
 }
 
 class _AddAccountState extends State<AddAccount> {
-  int _currentIndex = 1;
   final _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
   String errorMessage = "";
@@ -41,161 +40,159 @@ class _AddAccountState extends State<AddAccount> {
           child: Form(
             key: _formKey,
             child: Container(
-//            color: Colors.white,
               padding: EdgeInsets.all(24),
               child: Center(
                 child: Column(
                   children: <Widget>[
-                    SizedBox(
-                      height: 80,
-                    ),
-                    TextFormField(
-                      autofocus: false,
-                      obscureText: false,
-                      keyboardType: TextInputType.text,
-                      decoration: InputDecoration(
-                          labelText: "Bank Name",
-                          hintText: "Bank Name",
-                          labelStyle: TextStyle(
-                            color: Colors.black,
-                            fontSize: 16,
-                          ),
-                          border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(4)),
-                              borderSide: BorderSide(
-                                  width: 1,
-                                  color: Colors.green,
-                                  style: BorderStyle.solid))),
-                      validator: (val) =>
-                          val.isEmpty ? "Enter a valid bank name." : null,
-                      onChanged: (val) {
-                        setState(() {
-                          bankName = val;
-                        });
-                      },
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      autofocus: false,
-                      obscureText: false,
-                      keyboardType: TextInputType.text,
-                      decoration: InputDecoration(
-                          labelText: "Account Name",
-                          hintText: "Account Name",
-                          labelStyle: TextStyle(
-                            color: Colors.black,
-                            fontSize: 16,
-                          ),
-                          border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(4)),
-                              borderSide: BorderSide(
-                                  width: 1,
-                                  color: Colors.green,
-                                  style: BorderStyle.solid))),
-                      validator: (val) => val.length < 5
-                          ? "Enter a valid name matching account number."
-                          : null,
-                      onChanged: (val) {
-                        setState(() {
-                          accountName = val;
-                        });
-                      },
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      autofocus: false,
-                      obscureText: false,
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                          labelText: "Account Number",
-                          hintText: "Account Number",
-                          labelStyle: TextStyle(
-                            color: Colors.black,
-                            fontSize: 16,
-                          ),
-                          border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(4)),
-                              borderSide: BorderSide(
-                                  width: 1,
-                                  color: Colors.green,
-                                  style: BorderStyle.solid))),
-                      validator: (val) => val.length < 10
-                          ? "Enter a valid account number."
-                          : null,
-                      onChanged: (val) {
-                        setState(() {
-                          accountNumber = int.parse(val);
-                        });
-                      },
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
+                    SizedBox(height: 80),
+                    getBankName(),
+                    SizedBox(height: 10),
+                    getAccountName(),
+                    SizedBox(height: 10),
+                    getAccountNumber(),
+                    SizedBox(height: 20),
                     Text("Set as default account"),
-                    Switch(
-                      value: isDefault,
-                      onChanged: (value) {
-                        setState(() {
-                          isDefault = value;
-                        });
-                      },
-                      activeTrackColor: darkBlue(),
-                      activeColor: darkBlue(),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
+                    checkButton(),
+                    SizedBox(height: 20),
                     Text(
                       errorMessage,
                       style: TextStyle(color: Colors.red),
                     ),
                     SizedBox(height: 20),
-                    ButtonTheme(
-                      //elevation: 4,
-                      //color: Colors.green,
-                      minWidth: double.infinity,
-                      child: MaterialButton(
-                        onPressed: () async {
-                          if (_formKey.currentState.validate()) {
-                            Map data = {
-                              "customer_username": userBloc.user.userName,
-                              "bank": bankName,
-                              "account_name": accountName,
-                              "account_number": accountNumber,
-                              "is_default": isDefault,
-                            };
-                            bool wasSuccessful =
-                                await _auth.addBankAccount(data);
-                            if (wasSuccessful) {
-                              Navigator.pushNamedAndRemoveUntil(
-                                  context, "/accounts", (r) => false);
-                            } else {
-                              setState(() {
-                                errorMessage =
-                                    "An error has occured please try again";
-                              });
-                            }
-                          }
-                        },
-                        textColor: Colors.white,
-                        color: darkBlue(),
-                        height: 50,
-                        child: Text("Submit"),
-                      ),
-                    )
+                    getSubmitButton(userBloc.user.userName),
                   ],
                 ),
               ),
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget getBankName() {
+    return TextFormField(
+      autofocus: false,
+      obscureText: false,
+      keyboardType: TextInputType.text,
+      decoration: InputDecoration(
+          fillColor: Colors.white,
+          filled: true,
+          hintText: "Bank Name",
+          labelStyle: TextStyle(
+            color: Colors.black,
+            fontSize: 16,
+          ),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(4)),
+              borderSide: BorderSide(
+                  width: 1, color: Colors.green, style: BorderStyle.solid))),
+      validator: (val) => val.isEmpty ? "Enter a valid bank name." : null,
+      onChanged: (val) {
+        setState(() {
+          bankName = val;
+        });
+      },
+    );
+  }
+
+  Widget getAccountName() {
+    return TextFormField(
+      autofocus: false,
+      obscureText: false,
+      keyboardType: TextInputType.text,
+      decoration: InputDecoration(
+          fillColor: Colors.white,
+          filled: true,
+          hintText: "Account Name",
+          labelStyle: TextStyle(
+            color: Colors.black,
+            fontSize: 16,
+          ),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(4)),
+              borderSide: BorderSide(
+                  width: 1, color: Colors.green, style: BorderStyle.solid))),
+      validator: (val) =>
+          val.length < 5 ? "Enter a valid name matching account number." : null,
+      onChanged: (val) {
+        setState(() {
+          accountName = val;
+        });
+      },
+    );
+  }
+
+  Widget getAccountNumber() {
+    return TextFormField(
+      autofocus: false,
+      obscureText: false,
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+          fillColor: Colors.white,
+          filled: true,
+          hintText: "Account Number",
+          labelStyle: TextStyle(
+            color: Colors.black,
+            fontSize: 16,
+          ),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(4)),
+              borderSide: BorderSide(
+                  width: 1, color: Colors.green, style: BorderStyle.solid))),
+      validator: (val) =>
+          val.length < 10 ? "Enter a valid account number." : null,
+      onChanged: (val) {
+        setState(() {
+          accountNumber = int.parse(val);
+        });
+      },
+    );
+  }
+
+  Widget checkButton() {
+    return Switch(
+      value: isDefault,
+      onChanged: (value) {
+        setState(() {
+          isDefault = value;
+        });
+      },
+      activeTrackColor: darkBlue(),
+      activeColor: darkBlue(),
+    );
+  }
+
+  Widget getSubmitButton(String userName) {
+    return ButtonTheme(
+      //elevation: 4,
+      //color: Colors.green,
+      minWidth: double.infinity,
+      child: MaterialButton(
+        onPressed: () async {
+          if (_formKey.currentState.validate()) {
+            Map data = {
+              "customer_username": userName,
+              "bank": bankName,
+              "account_name": accountName,
+              "account_number": accountNumber,
+              "is_default": isDefault,
+            };
+            bool wasSuccessful = await _auth.addBankAccount(data);
+            if (wasSuccessful) {
+              Navigator.pushNamedAndRemoveUntil(
+                  context, "/accounts", (r) => false);
+            } else {
+              setState(() {
+                errorMessage = "An error has occured please try again";
+              });
+            }
+          }
+        },
+        textColor: Colors.white,
+        color: darkBlue(),
+        height: 50,
+        child: Text("Submit"),
       ),
     );
   }
