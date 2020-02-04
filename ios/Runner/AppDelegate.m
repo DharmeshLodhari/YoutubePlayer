e7bd116bca2951cb77e65433fc5125af2a0dfefc
abi <abiola.rasheed@novastonemedia.com>
2020-02-04 11:13:19 +0000

    Integrated crashlytics for android

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,12 +1,15 @@
-#import "AppDelegate.h"
-#import "GeneratedPluginRegistrant.h"
+#include "AppDelegate.h"
+#include "GeneratedPluginRegistrant.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  if (@available(iOS 10.0, *)) {
+    [UNUserNotificationCenter currentNotificationCenter].delegate =
+        (id<UNUserNotificationCenterDelegate>)self;
+  }
   [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
+import 'dart:async';
+
 import 'package:Slydo/data/state_notifier.dart';
 import 'package:Slydo/screens/route_generator.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
-void main() => runApp(
+
+void main() {
+
+  //Crashlytics.instance.enableInDevMode = true;
+
+  // Pass all uncaught errors to Crashlytics.
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
+
+  runZoned(() {
+    runApp(
       MultiProvider(
           providers: [
             ChangeNotifierProvider<UserBloc>.value(
@@ -22,3 +34,5 @@ void main() => runApp(
             debugShowCheckedModeBanner: false,
           )),
     );
+  }, onError: Crashlytics.instance.recordError);
+}
