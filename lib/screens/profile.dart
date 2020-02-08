bc2dacb6cf146806d23bf11f550442fb86b43479
abi <abiola.rasheed@novastonemedia.com>
2020-02-08 11:24:09 +0000

    Resize profile avatar on home screen

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -79,8 +79,8 @@ class _ProfileState extends State<Profile> {
                 colorBlendMode: BlendMode.darken,
                 fit: BoxFit.fitWidth,
                 filterQuality: FilterQuality.high,
-                loadingBuilder:
-                    (BuildContext context, Widget child, ImageChunkEvent loadingProgress) {
+                loadingBuilder: (BuildContext context, Widget child,
+                    ImageChunkEvent loadingProgress) {
                   if (loadingProgress == null) return child;
                   return Center(
                     child: CircularProgressIndicator(
@@ -104,13 +104,16 @@ class _ProfileState extends State<Profile> {
                 FlatButton.icon(
                     onPressed: () {
                       Clipboard.setData(new ClipboardData(
-                          text: baseUrl + "/api/v1/customer/" + userBloc.user.userName));
+                          text: baseUrl +
+                              "/api/v1/customer/" +
+                              userBloc.user.userName));
                       key.currentState.showSnackBar(SnackBar(
                         content: new Text("Coped!"),
                       ));
                     },
                     icon: Icon(Icons.settings, color: Colors.black),
-                    label: Text('Copy Url', style: TextStyle(color: Colors.black, fontSize: 14))),
+                    label: Text('Copy Url',
+                        style: TextStyle(color: Colors.black, fontSize: 14))),
               ],
             ),
           ],
@@ -126,13 +129,14 @@ class _ProfileState extends State<Profile> {
       child: MaterialButton(
         elevation: 4.0,
         onPressed: () {
-          Navigator.of(context)
-              .pushNamed('/send-payment', arguments: <String, bool>{'isFromProfile': true});
+          Navigator.of(context).pushNamed('/send-payment',
+              arguments: <String, bool>{'isFromProfile': true});
         },
         textColor: Colors.white,
         color: darkBlue(),
         height: 50,
-        child: Text("Make a Payment"), // change this to make payment request button to
+        child: Text(
+            "Make a Payment"), // change this to make payment request button to
       ),
     );
   }
@@ -163,7 +167,7 @@ class _ProfileState extends State<Profile> {
 
   Widget displayUserAvatar(userBloc) {
     return Padding(
-      padding: const EdgeInsets.all(4.0),
+      padding: const EdgeInsets.all(10.0),
       child: ClipOval(
         child: Image.network(
           userBloc.user.avatar,
