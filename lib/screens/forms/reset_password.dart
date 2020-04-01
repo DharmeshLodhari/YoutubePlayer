aa68fd008249b4a9828590b353f587bf327014be
BlackStriker99 <sakariyabrijesh5555@gmail.com>
2020-04-01 18:36:59 +0530

    "Error solved by bitrise"

--- lib/screens/forms/reset_password.dart
+++ lib/screens/forms/reset_password.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 import '../colors.dart';
 
+// ignore: must_be_immutable
 class ResetPassword extends StatefulWidget {
   var arguments;
 
--- lib/screens/messagelist.dart
+++ lib/screens/messagelist.dart
@@ -464,4 +464,3 @@ class VerticalListItem extends StatelessWidget {
   }
 }
 
-enum LoadMoreData { LOADING, STABLE }
