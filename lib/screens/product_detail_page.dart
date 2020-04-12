45add0f0e49121af3d14388576697f97b242b7d5
Abiola Rasheed <rasheed.abiola3@gmail.com>
2020-04-12 17:06:34 +0100

    Cleanup product page

--- lib/screens/product_detail_page.dart
+++ lib/screens/product_detail_page.dart
@@ -28,7 +28,7 @@ class _ProductDetailPageState extends State<ProductDetailPage>
           icon: Icon(
             Icons.chevron_left,
             size: 40.0,
-            color: Colors.black,
+            color: Colors.white,
           ),
           onPressed: () {
             Navigator.of(context).pop();
@@ -38,12 +38,11 @@ class _ProductDetailPageState extends State<ProductDetailPage>
         title: Text(
           "PRODUCT DETAIL",
           style: TextStyle(
-            color: Colors.black,
+            color: Colors.white,
           ),
         ),
       ),
       body: _buildProductDetailsPage(context),
-      bottomNavigationBar: _buildBottomNavigationBar(),
     );
   }
 
@@ -227,7 +226,7 @@ class _ProductDetailPageState extends State<ProductDetailPage>
               ),
               Tab(
                 child: Text(
-                  "MATERIAL & CARE",
+                  "Seller Other Products",
                   style: TextStyle(
                     color: Colors.black,
                   ),
@@ -248,46 +247,12 @@ class _ProductDetailPageState extends State<ProductDetailPage>
                   ),
                 ),
                 Text(
-                  "86% acrylic, 9% polyster, 1% metallic yarn Hand-wash cold",
+                  "Coming soon!",
                   style: TextStyle(
                     color: Colors.black,
                   ),
                 )
               ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  _buildBottomNavigationBar() {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 50.0,
-      child: Row(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          Flexible(
-            flex: 2,
-            child: RaisedButton(
-              onPressed: () {},
-              color: Colors.greenAccent,
-              child: Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    SizedBox(
-                      width: 4.0,
-                    ),
-                    Text(
-                      "Pay Now",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ],
-                ),
-              ),
             ),
           ),
         ],
