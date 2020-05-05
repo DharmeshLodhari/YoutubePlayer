0fda3848ef96720b8b2fd79dbd18929b19326a74
BlackStriker99 <sakariyabrijesh5555@gmail.com>
2020-05-05 17:42:18 +0530

    VERSION 1.1.0.1 GRAPH: GRAPH INDEX ADDED TO 1..7 & PAYOUT ISSUE SOLVED & AMOUNT CONVERTED INTO WORD IN GRAPH - By PS

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'dart:io';
-import 'dart:math';
 
 import 'package:Slydo/data/database_helper.dart';
 import 'package:Slydo/models/message.dart';
@@ -1432,63 +1431,63 @@ class AuthService {
     var response = await http.get(url, headers: headers);
     if (response.statusCode == 200) {
       var jsonData = json.decode(response.body);
-//      return jsonData;
-      var randomize = Random();
-      var result = {
-        "results": {
-          "categories": [
-            {
-              "category": "Family",
-              "amount": 1000.00,
-              "url": "assets/images/category/bills.png"
-            },
-            {
-              "category": "Shopping",
-              "amount": 12674.00,
-              "url": "assets/images/category/bills.png"
-            },
-            {
-              "category": "Transportation",
-              "amount": 1000.00,
-              "url": "assets/images/category/bills.png"
-            },
-            {
-              "category": "Uncategorized",
-              "amount": 300.00,
-              "url": "assets/images/category/bills.png"
-            }
-          ],
-          "week": [
-            {"day": 1, "amount": randomize.nextInt(1000)},
-            {"day": 2, "amount": randomize.nextInt(1000)},
-            {"day": 3, "amount": randomize.nextInt(1000)},
-            {"day": 4, "amount": randomize.nextInt(1000)},
-            {"day": 5, "amount": randomize.nextInt(1000)},
-            {"day": 6, "amount": randomize.nextInt(1000)},
-            {"day": 7, "amount": randomize.nextInt(1000)}
-          ],
-          "income": [
-            {"day": 1, "amount": randomize.nextInt(1000)},
-            {"day": 7, "amount": randomize.nextInt(1000)},
-            {"day": 2, "amount": randomize.nextInt(1000)},
-            {"day": 3, "amount": randomize.nextInt(1000)},
-            {"day": 4, "amount": randomize.nextInt(1000)},
-            {"day": 5, "amount": randomize.nextInt(1000)},
-            {"day": 6, "amount": randomize.nextInt(1000)},
-          ],
-          "expenditure": [
-            {"day": 7, "amount": randomize.nextInt(600)},
-            {"day": 1, "amount": randomize.nextInt(770)},
-            {"day": 2, "amount": randomize.nextInt(1000)},
-            {"day": 3, "amount": randomize.nextInt(500)},
-            {"day": 4, "amount": randomize.nextInt(200)},
-            {"day": 6, "amount": randomize.nextInt(1000)},
-            {"day": 5, "amount": randomize.nextInt(600)},
-          ]
-        }
-      };
-
-      return result;
+      return jsonData;
+//      var randomize = Random();
+//      var result = {
+//        "results": {
+//          "categories": [
+//            {
+//              "category": "Family",
+//              "amount": 1000.00,
+//              "url": "assets/images/category/bills.png"
+//            },
+//            {
+//              "category": "Shopping",
+//              "amount": 12674.00,
+//              "url": "assets/images/category/bills.png"
+//            },
+//            {
+//              "category": "Transportation",
+//              "amount": 1000.00,
+//              "url": "assets/images/category/bills.png"
+//            },
+//            {
+//              "category": "Uncategorized",
+//              "amount": 300.00,
+//              "url": "assets/images/category/bills.png"
+//            }
+//          ],
+//          "week": [
+//            {"day": 1, "amount": randomize.nextInt(1000)},
+//            {"day": 2, "amount": randomize.nextInt(1000)},
+//            {"day": 3, "amount": randomize.nextInt(1000)},
+//            {"day": 4, "amount": randomize.nextInt(1000)},
+//            {"day": 5, "amount": randomize.nextInt(1000)},
+//            {"day": 6, "amount": randomize.nextInt(1000)},
+//            {"day": 7, "amount": randomize.nextInt(1000)}
+//          ],
+//          "income": [
+//            {"day": 1, "amount": randomize.nextInt(1000)},
+//            {"day": 7, "amount": randomize.nextInt(1000)},
+//            {"day": 2, "amount": randomize.nextInt(1000)},
+//            {"day": 3, "amount": randomize.nextInt(1000)},
+//            {"day": 4, "amount": randomize.nextInt(1000)},
+//            {"day": 5, "amount": randomize.nextInt(1000)},
+//            {"day": 6, "amount": randomize.nextInt(1000)},
+//          ],
+//          "expenditure": [
+//            {"day": 7, "amount": randomize.nextInt(600)},
+//            {"day": 1, "amount": randomize.nextInt(770)},
+//            {"day": 2, "amount": randomize.nextInt(1000)},
+//            {"day": 3, "amount": randomize.nextInt(500)},
+//            {"day": 4, "amount": randomize.nextInt(200)},
+//            {"day": 6, "amount": randomize.nextInt(1000)},
+//            {"day": 5, "amount": randomize.nextInt(600)},
+//          ]
+//        }
+//      };
+//
+//      return result;
     } else {
       var jsonData = json.decode(response.body);
       throw jsonData;
