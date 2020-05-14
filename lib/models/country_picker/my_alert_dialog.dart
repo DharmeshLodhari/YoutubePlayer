55287678c52bd6911e7afeda3561d6c43947c9c0
BlackStriker99 <sakariyabrijesh5555@gmail.com>
2020-05-14 19:51:10 +0530

    VERSION 1.2.0.1 ADD PRODUCT: ADDRESS ADDED

--- lib/models/country_picker/my_alert_dialog.dart
+++ lib/models/country_picker/my_alert_dialog.dart
@@ -105,7 +105,7 @@ class MyAlertDialog<T> extends StatelessWidget {
             new EdgeInsets.fromLTRB(
                 24.0, 24.0, 24.0, isDividerEnabled ? 20.0 : 0.0),
         child: new DefaultTextStyle(
-          style: Theme.of(context).textTheme.headline,
+          style: Theme.of(context).textTheme.headline6,
           child: new Semantics(child: title, namesRoute: true),
         ),
       ));
@@ -121,14 +121,11 @@ class MyAlertDialog<T> extends StatelessWidget {
           label = semanticLabel ??
               MaterialLocalizations.of(context)?.alertDialogLabel;
           break;
-//        case TargetPlatform.linux:
-//          // TODO: Handle this case.
-//          break;
-//        case TargetPlatform.macOS:
-//          // TODO: Handle this case.
-//          break;
-//        case TargetPlatform.windows:
-//          // TODO: Handle this case.
+        case TargetPlatform.linux:
+          break;
+        case TargetPlatform.macOS:
+          break;
+        case TargetPlatform.windows:
           break;
       }
     }
