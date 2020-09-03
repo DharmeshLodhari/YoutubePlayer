9baae481432f3c376e1c224a718c0bc9ca9ad6e8
Brijesh Sakariya <sakariyabrijesh5555@gmail.com>
2020-09-03 18:33:52 +0530

    BASKET UI INCOMPLETE QTY CHANGE

--- lib/screens/tiles/shopping_cart_tile.dart
+++ lib/screens/tiles/shopping_cart_tile.dart
@@ -292,14 +292,13 @@ class _ShoppingCartTileForServiceState
           colorBlendMode: BlendMode.darken,
           fit: BoxFit.fitWidth,
           filterQuality: FilterQuality.high,
-          placeholder: (context, url) =>
-          service.serverImages.isNotEmpty
+          placeholder: (context, url) => service.serverImages.isNotEmpty
               ? Icon(Icons.widgets)
               : CircularProgressIndicator(
-            strokeWidth: 2.5,
-            valueColor: AlwaysStoppedAnimation(Colors.white),
-            backgroundColor: lightBlue(),
-          ),
+                  strokeWidth: 2.5,
+                  valueColor: AlwaysStoppedAnimation(Colors.white),
+                  backgroundColor: lightBlue(),
+                ),
         ),
       ),
     );
