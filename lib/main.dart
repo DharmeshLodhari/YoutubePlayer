4ab2cdbb317cfdc69ca5a039a935369fad816475
BlackStriker99 <sakariyabrijesh5555@gmail.com>
2021-06-04 16:22:17 +0530

    Fastlane integrated with CI for android Completed

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import 'package:Slydo/utils/colors.dart';
 import 'package:Slydo/utils/global_key.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart' show kDebugMode;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -36,6 +37,12 @@ void main() async {
   await Firebase.initializeApp();
 
   AwesomeNotificationService().init();
+
+  if (kDebugMode) {
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+  } else {
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+  }
 
   // Pass all uncaught errors to Crashlytics.
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
