254607b65e0c66e993c4f6d95e7f30302b7b13ce
Franklin Oladipo <ibukunoladipo2015@gmail.com>
2022-05-06 21:21:06 +0100

    Changed sdk version in pubspec

--- lib/widget/image_crop.dart
+++ lib/widget/image_crop.dart
@@ -8,35 +8,36 @@ class ImageCrop {
   Future<String?> cropImage(String filePath) async {
     debugPrint("Selected image => $filePath");
 
-    File? croppedImage = await ImageCropper().cropImage(
-        sourcePath: filePath,
-        aspectRatioPresets: Platform.isAndroid
-            ? [
-                CropAspectRatioPreset.square,
-                CropAspectRatioPreset.ratio3x2,
-                CropAspectRatioPreset.original,
-                CropAspectRatioPreset.ratio4x3,
-                CropAspectRatioPreset.ratio16x9
-              ]
-            : [
-                CropAspectRatioPreset.original,
-                CropAspectRatioPreset.square,
-                CropAspectRatioPreset.ratio3x2,
-                CropAspectRatioPreset.ratio4x3,
-                CropAspectRatioPreset.ratio5x3,
-                CropAspectRatioPreset.ratio5x4,
-                CropAspectRatioPreset.ratio7x5,
-                CropAspectRatioPreset.ratio16x9
-              ],
-        androidUiSettings: AndroidUiSettings(
+    CroppedFile? croppedImage = await ImageCropper().cropImage(
+      sourcePath: filePath,
+      aspectRatioPresets: Platform.isAndroid
+          ? [
+              CropAspectRatioPreset.square,
+              CropAspectRatioPreset.ratio3x2,
+              CropAspectRatioPreset.original,
+              CropAspectRatioPreset.ratio4x3,
+              CropAspectRatioPreset.ratio16x9
+            ]
+          : [
+              CropAspectRatioPreset.original,
+              CropAspectRatioPreset.square,
+              CropAspectRatioPreset.ratio3x2,
+              CropAspectRatioPreset.ratio4x3,
+              CropAspectRatioPreset.ratio5x3,
+              CropAspectRatioPreset.ratio5x4,
+              CropAspectRatioPreset.ratio7x5,
+              CropAspectRatioPreset.ratio16x9
+            ],
+      uiSettings: [
+        AndroidUiSettings(
           statusBarColor: navyBlue,
           activeControlsWidgetColor: navyBlue,
           lockAspectRatio: false,
           initAspectRatio: CropAspectRatioPreset.original,
         ),
-        iosUiSettings: IOSUiSettings(
-          aspectRatioLockEnabled: false,
-        ));
+        IOSUiSettings(aspectRatioLockEnabled: false)
+      ],
+    );
 
     if (croppedImage != null) {
       debugPrint("Cropped image => ${croppedImage.path}");
