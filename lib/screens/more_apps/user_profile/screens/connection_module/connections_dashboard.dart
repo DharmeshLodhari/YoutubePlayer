1514f3311c3294275dbbb4fefccf96682f2283c9
Franklin Oladipo <ibukunoladipo2015@gmail.com>
2022-09-14 15:35:53 +0100

    Added suggestions tab to search page

--- lib/screens/more_apps/user_profile/screens/connection_module/connections_dashboard.dart
+++ lib/screens/more_apps/user_profile/screens/connection_module/connections_dashboard.dart
@@ -285,7 +285,23 @@ class _ConnectionDashboardState extends State<ConnectionDashboard> {
         ConnectionList(),
         ConnectionRequestList(),
         BlockedList(),
+        ChannelList(),
       ],
     );
   }
 }
+
+class ChannelList extends StatefulWidget {
+  const ChannelList({Key? key}) : super(key: key);
+
+  @override
+  _ChannelListState createState() => _ChannelListState();
+}
+
+class _ChannelListState extends State<ChannelList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
