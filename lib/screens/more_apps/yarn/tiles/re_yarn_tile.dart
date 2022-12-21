4de189ab71f72355758b2b8844c09b1535b4ab0e
Mfon <uwahsheedo@gmail.com>
2022-12-21 19:12:16 +0100

    changes after repulling

--- lib/screens/more_apps/yarn/tiles/re_yarn_tile.dart
+++ lib/screens/more_apps/yarn/tiles/re_yarn_tile.dart
@@ -73,7 +73,7 @@ class _ReYarnTileState extends State<ReYarnTile> {
         ],
         _buildPostDescription(),
         SizedBox(
-          height: 10,
+          height: 9,
         ),
         _buildTagsAndViewerRow(),
         if (isAttachmentPresent && widget.yarn.attachment != null) ...[
