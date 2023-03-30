a8054847f9c6be1c0e6b5fe5fc4968bb0ec06e83
Yusuf Adefolahan <yusufadefolahan@gmail.com>
2023-03-30 08:26:04 +0000

    Renamed channel to channels on the profile

--- lib/screens/more_apps/user_profile/screens/user_profile_module_new/profile_template/business_profile_screen.dart
+++ lib/screens/more_apps/user_profile/screens/user_profile_module_new/profile_template/business_profile_screen.dart
@@ -71,7 +71,7 @@ class _BusinessProfileScreenState extends State<BusinessProfileScreen>
           apiCall: () async => await fetchPostData(searchedUserName),
         ),
         UserTab(
-          label: "Channel",
+          label: "Channels",
           child: channelTab(searchedUserName),
           apiCall: () async => await fetchChannelData(searchedUserName),
         ),
