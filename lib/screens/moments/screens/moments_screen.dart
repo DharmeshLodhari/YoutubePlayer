de91ff0ac43626159d8616cabc1aa196c7ef6028
Mfon Uwah <uwahsheedo@gmail.com>
2023-08-21 21:27:14 +0000

    Slydo/moment fix

--- lib/screens/moments/screens/moments_screen.dart
+++ lib/screens/moments/screens/moments_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:developer';
+
 import 'package:Slydo/screens/moments/models/moments_model.dart';
 import 'package:Slydo/screens/moments/screens/create_moment_screen.dart';
+import 'package:Slydo/screens/moments/screens/moment_detail/custom_story_view.dart';
 import 'package:Slydo/screens/moments/screens/moment_detail/moment_detail_page.dart';
+import 'package:Slydo/screens/moments/screens/moment_detail/story_moment.dart';
 import 'package:Slydo/screens/moments/screens/moments_service.dart';
 import 'package:Slydo/screens/moments/utils.dart';
 import 'package:Slydo/utils/extensions.dart';
@@ -15,6 +19,8 @@ import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:shimmer/shimmer.dart';
+import 'package:story_view/controller/story_controller.dart';
+import 'package:story_view/widgets/story_view.dart';
 import '../../../data/state_notifier.dart';
 import '../../../locale/app_localization.dart';
 import '../../../locator.dart';
@@ -210,7 +216,15 @@ class _MomentsScreenState extends State<MomentsScreen> {
         }
 
         debugPrint('EXPLORE MOM :: $exploreMomentsList');
-
+        // for (int i = 0; i < exploreMomentsList.length; i++) {
+        //   for (int j = 0; j <= exploreMomentsList[i].moments!.length; j++) {
+        //     setState(() {
+        //       listOfMoments?.add(exploreMomentsList[i].moments![j]);
+        //     });
+        //   }
+        //   print('beeetttttt....${exploreMomentsList[i].moments!.length}');
+        //   print('list of momentssss....${listOfMoments!.length}');
+        // }
         if (mounted) setState(() {});
 
         debugPrint(
@@ -465,7 +479,7 @@ class _MomentsScreenState extends State<MomentsScreen> {
               style: TextStyle(
                 fontWeight: FontWeight.w600,
                 color: blackFont,
-                fontSize: 16,
+                // fontSize: 16,P
               ),
             ),
             SizedBox(width: 10),
@@ -737,13 +751,14 @@ class _ContactMomentsCardState extends State<ContactMomentsCard> {
   }
 }
 
-class ExploreMomentsCard extends StatelessWidget {
+class ExploreMomentsCard extends StatefulWidget {
   // This index is the position of the 'ExploreMomentsCard' in the list 0f explore moments.
   final int index;
   final Function()? onTap;
   final bool showProfileAvatar;
   final List<ExploreMomentsModel> exploreMomentsModelList;
-  const ExploreMomentsCard(
+
+  ExploreMomentsCard(
       {Key? key,
       this.onTap,
       this.showProfileAvatar =
@@ -753,20 +768,62 @@ class ExploreMomentsCard extends StatelessWidget {
       : super(key: key);
 
   @override
+  State<ExploreMomentsCard> createState() => _ExploreMomentsCardState();
+}
+
+class _ExploreMomentsCardState extends State<ExploreMomentsCard> {
+  final storyController = StoryController();
+
+  List<Shiddo> storyItems = [];
+
+  ExploreMomentsModel? exploreMoment;
+  MomentsModel? sigleMoment;
+
+  @override
+  void initState() {
+    widget.exploreMomentsModelList[widget.index].moments!.map((e) {
+      sigleMoment = e;
+      if (e.mediaType == 'image') {
+        storyItems.add(Shiddo.pageImage(
+            url: e.media!,
+            controller: storyController,
+            duration: Duration(seconds: 10),
+            momentsModel: e));
+      }
+      if (e.mediaType == 'video') {
+        storyItems.add(Shiddo.pageVideo(e.media!,
+            controller: storyController,
+            duration: Duration(seconds: 30),
+            momentsModel: e));
+      }
+    }).toList();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-        if (onTap != null) {
-          onTap!();
+        if (widget.onTap != null) {
+          widget.onTap!();
         } else {
           NavigationUtil.push(
             context,
-            screen: MomentsDetailsScreen(
-              indexOfMoment: index,
-              momentsModelList:
-                  exploreMomentsModelList.map((e) => e.moments!).toList(),
+            screen: StoryMomentScreen(
+              controller: storyController,
+              storyItems: storyItems,
+              currentMoment: sigleMoment,
             ),
           );
+          ////
+          // NavigationUtil.push(
+          //   context,
+          //   screen: MomentsDetailsScreen(
+          //     indexOfMoment: index,
+          //     momentsModelList:
+          //         exploreMomentsModelList.map((e) => e.moments!).toList(),
+          //   ),
+          // );
         }
       },
       child: Card(
@@ -778,17 +835,19 @@ class ExploreMomentsCard extends StatelessWidget {
           fit: StackFit.expand,
           children: [
             _getMediaRenderer(
-              momentModel: exploreMomentsModelList[index].moments!.first,
+              momentModel:
+                  widget.exploreMomentsModelList[widget.index].moments!.first,
               context: context,
             ),
-            showProfileAvatar
+            widget.showProfileAvatar
                 ? Align(
                     alignment: Alignment.topLeft,
                     child: Padding(
                       padding: const EdgeInsets.only(left: 8.0, top: 10),
                       child: MomentsUtils().getUserProfilePic(
-                          exploreMomentsModelList[index].avatar!,
-                          exploreMomentsModelList[index].ownerName!),
+                          widget.exploreMomentsModelList[widget.index].avatar!,
+                          widget.exploreMomentsModelList[widget.index]
+                              .ownerName!),
                     ),
                   )
                 : SizedBox.shrink(),
@@ -801,7 +860,9 @@ class ExploreMomentsCard extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     userNameWithVerifiedIcon(
-                      name: exploreMomentsModelList[index].ownerName ?? '',
+                      name: widget.exploreMomentsModelList[widget.index]
+                              .ownerName ??
+                          '',
                       isVerified: false,
                       textStyle: TextStyle(
                         fontSize: 12,
@@ -818,8 +879,10 @@ class ExploreMomentsCard extends StatelessWidget {
                     ),
                     Text(
                       getFormattedViewCount(
-                          noOfViews:
-                              exploreMomentsModelList[index].moments![0].views),
+                          noOfViews: widget
+                              .exploreMomentsModelList[widget.index]
+                              .moments![0]
+                              .views),
                       style: TextStyle(
                         fontSize: 12,
                         shadows: [
@@ -838,7 +901,9 @@ class ExploreMomentsCard extends StatelessWidget {
                 ),
               ),
             ),
-            exploreMomentsModelList[index].moments!.first.mediaType == 'video'
+            widget.exploreMomentsModelList[widget.index].moments!.first
+                        .mediaType ==
+                    'video'
                 ? Align(
                     alignment: Alignment.topRight,
                     child: Padding(
