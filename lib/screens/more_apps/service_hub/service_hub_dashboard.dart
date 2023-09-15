2cf1e1e894ac2b7c08de4c2600d81eda3a1bba6c
Mfon Uwah <uwahsheedo@gmail.com>
2023-09-15 14:46:21 +0000

    Slydo/highlighted bugs

--- lib/screens/more_apps/service_hub/service_hub_dashboard.dart
+++ lib/screens/more_apps/service_hub/service_hub_dashboard.dart
@@ -9,7 +9,6 @@ import 'package:Slydo/utils/colors.dart';
 import 'package:Slydo/utils/slydo_app_icon_icons.dart';
 import 'package:colorful_safe_area/colorful_safe_area.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 
 import '../../../utils/util.dart';
