5e4e5f0a0578c1184e866abc2d23fb9b20d1c2dc
Yusuf Adefolahan <a.yusuf@capitalsage.ng>
2023-10-16 09:45:26 +0100

    adjusted click from chat to profile page, change icon for menu

--- lib/screens/more_apps/messaging/chat/screens/chat_screen_group_messages.dart
+++ lib/screens/more_apps/messaging/chat/screens/chat_screen_group_messages.dart
@@ -69,6 +69,7 @@ import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_sound/flutter_sound.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:giphy_picker/giphy_picker.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:images_picker/images_picker.dart';
@@ -1341,37 +1342,20 @@ class _ChatScreenGroupMessageState extends State<ChatScreenGroupMessage>
         },
         child: Platform.isAndroid
             ? mainStack()
-            : Banner(
-                message: 'BETA',
-                child: mainStack(),
-                location: BannerLocation.topEnd,
-              ),
+            // : Banner(
+            //     message: 'BETA',
+            //     child: mainStack(),
+            //     location: BannerLocation.topEnd,
+            //   ),
+            : mainStack()
       ),
     );
   }
 
   Widget appBar() {
-    return AppBar(
-      elevation: 0,
-      backgroundColor: Colors.white,
-      titleSpacing: 0,
-      automaticallyImplyLeading: false,
-      leading: IconButton(
-        icon: Icon(
-          Icons.keyboard_arrow_left,
-          color: navyBlue,
-          size: 28,
-        ),
-        onPressed: () {
-          disposeAudioPlayers();
-          mainSocketProvider!.removeStreamSubscription(streamSubscription);
-          mainSocketProvider!.currentConversationId = null;
-          mainSocketProvider!.isChatOnScreen = false;
-          Navigator.pop(context);
-        },
-      ),
-      leadingWidth: 40,
-      title: GestureDetector(
+    return PreferredSize(
+      preferredSize: Size.fromHeight(kToolbarHeight),
+      child: GestureDetector(
         onTap: () async {
           stopShakeDetector();
           if (chatConversation!.isGroupConversation!) {
@@ -1385,83 +1369,122 @@ class _ChatScreenGroupMessageState extends State<ChatScreenGroupMessage>
           }
           setupShakeDetector();
         },
-        child: Row(
-          children: [
-            StreamBuilder<Object>(
-                initialData: false,
-                stream: ChatMessageSynchronizer().getChatMessageStream,
-                builder: (context, snapshot) {
-                  if (snapshot.data == true) {
-                    ChatMessageSynchronizer().setStreamFalse();
-                    getMissedMessageFromDB();
-                  }
-                  return getUserIcon();
-                }),
-            SizedBox(width: 12),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  userNameWithVerifiedIcon(
-                    name: chatConversation?.fullName != null
-                        ? chatConversation!.fullName!
-                        : '',
-                    isVerified: chatConversation?.isVerified,
-                    verifiedIconColor: verifyGreen,
-                  ),
-                  Text(
-                    chatConversation != null
-                        ? isRecipientTyping
-                            ? typingMessage!
-                            : isOtherUserRecordingAudio
-                                ? "recording audio"
-                                : userStatus
-                        : "", //"Online",
-                    style: TextStyle(
-                        color: isRecipientTyping || isOtherUserRecordingAudio
-                            ? naturalGreen
-                            : darkGrey,
-                        fontSize: 10,
-                        fontWeight: FontWeight.w400),
-                    overflow: TextOverflow.fade,
-                    softWrap: false,
-                    maxLines: 1,
-                  ),
-                ],
-              ),
+        child: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          titleSpacing: 0,
+          automaticallyImplyLeading: false,
+          leading: IconButton(
+            icon: Icon(
+              Icons.keyboard_arrow_left,
+              color: navyBlue,
+              size: 28,
             ),
-          ],
-        ),
-      ),
-      actions: [
-        if (chatConversation != null &&
-            chatConversation!.isGroupConversation!) ...[
-          GestureDetector(
+            onPressed: () {
+              disposeAudioPlayers();
+              mainSocketProvider!.removeStreamSubscription(streamSubscription);
+              mainSocketProvider!.currentConversationId = null;
+              mainSocketProvider!.isChatOnScreen = false;
+              Navigator.pop(context);
+            },
+          ),
+          leadingWidth: 40,
+          title: GestureDetector(
             onTap: () async {
               stopShakeDetector();
               if (chatConversation!.isGroupConversation!) {
-                // navigateToGroupDetailScreen();
-
-                showChannelMenuList();
+                Navigator.pushNamed(context, Routes.USER_PROFILE, arguments: {
+                  "searchedUserName": chatConversation!.conversationId,
+                  "channel": chatConversation!.fullName,
+                });
               } else {
                 await Navigator.pushNamed(context, Routes.USER_PROFILE,
-                    arguments: {
-                      "searchedUserName": chatConversation!.userName
-                    });
+                    arguments: {"searchedUserName": chatConversation!.userName});
               }
               setupShakeDetector();
             },
-            child: Padding(
-              padding: const EdgeInsets.only(right: 10.0),
-              child: Icon(
-                Icons.settings,
-                color: blackFont,
-                size: 22,
-              ),
+            child: Row(
+              children: [
+                StreamBuilder<Object>(
+                    initialData: false,
+                    stream: ChatMessageSynchronizer().getChatMessageStream,
+                    builder: (context, snapshot) {
+                      if (snapshot.data == true) {
+                        ChatMessageSynchronizer().setStreamFalse();
+                        getMissedMessageFromDB();
+                      }
+                      return getUserIcon();
+                    }),
+                SizedBox(width: 12),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      userNameWithVerifiedIcon(
+                        name: chatConversation?.fullName != null
+                            ? chatConversation!.fullName!
+                            : '',
+                        isVerified: chatConversation?.isVerified,
+                        verifiedIconColor: verifyGreen,
+                      ),
+                      Text(
+                        chatConversation != null
+                            ? isRecipientTyping
+                                ? typingMessage!
+                                : isOtherUserRecordingAudio
+                                    ? "recording audio"
+                                    : userStatus
+                            : "", //"Online",
+                        style: TextStyle(
+                            color: isRecipientTyping || isOtherUserRecordingAudio
+                                ? naturalGreen
+                                : darkGrey,
+                            fontSize: 10,
+                            fontWeight: FontWeight.w400),
+                        overflow: TextOverflow.fade,
+                        softWrap: false,
+                        maxLines: 1,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
-        ]
-      ],
+          actions: [
+            if (chatConversation != null &&
+                chatConversation!.isGroupConversation!) ...[
+              GestureDetector(
+                onTap: () async {
+                  stopShakeDetector();
+                  if (chatConversation!.isGroupConversation!) {
+                    // navigateToGroupDetailScreen();
+
+                    showChannelMenuList();
+                  } else {
+                    await Navigator.pushNamed(context, Routes.USER_PROFILE,
+                        arguments: {
+                          "searchedUserName": chatConversation!.userName
+                        });
+                  }
+                  setupShakeDetector();
+                },
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 20.0),
+                  // child: Icon(
+                  //   Icons.menu,
+                  //   color: blackFont,
+                  //   size: 22,
+                  // ),
+                  child: SvgPicture.asset(
+                    'assets/images/menu.svg',
+                  ),
+                ),
+              ),
+            ]
+          ],
+        ),
+      ),
     );
   }
 
