e3ffccff2983cfe9b161c82be63e10596a219dff
brijeshsakariya <sakariyabrijesh5555@gmail.com>
2024-04-16 22:37:27 +0530

    rider delivery poligone inProgress

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 echo BUILDING IOS SLYDO
 cd .. && cd ios && rm -rf Pods && rm -rf .symlinks && rm -rf Podfile.lock && cd .. && rm -rf pubspec.lock && fvm flutter clean && fvm flutter pub get && cd ios && pod install && cd ..
 fvm flutter build xcarchive
-open /Users/brijeshsakariya/StudioProjects/slydo/build/ios/archive/Runner.xcarchive
+open /Users/brijeshsakariya/StudioProjects/s  `lydo/build/ios/archive/Runner.xcarchive
