3a03813611e29cb8f9b738aef468a1d12230a035
brijeshsakariya <sakariyabrijesh5555@gmail.com>
2024-04-16 23:29:15 +0530

    VERSION UPDATED

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 echo BUILDING IOS SLYDO
 cd .. && cd ios && rm -rf Pods && rm -rf .symlinks && rm -rf Podfile.lock && cd .. && rm -rf pubspec.lock && fvm flutter clean && fvm flutter pub get && cd ios && pod install && cd ..
 fvm flutter build xcarchive
-open /Users/macmini8/Documents/Project/slydo/build/ios/archive/Runner.xcarchive
+#open /Users/macmini8/Documents/Project/slydo/build/ios/archive/Runner.xcarchive
+open /Users/brijeshsakariya/StudioProjects/slydo/build/ios/archive/Runner.xcarchive
