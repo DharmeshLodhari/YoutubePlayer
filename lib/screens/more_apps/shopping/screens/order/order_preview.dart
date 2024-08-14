b2bef6e044b694545a9811337583cb3173004abf
HemaliSoliya <hemali.srashtasoft@gmail.com>
2024-08-14 19:46:08 +0530

    - Receipt screen app bar added printer

--- lib/screens/more_apps/shopping/screens/order/order_preview.dart
+++ lib/screens/more_apps/shopping/screens/order/order_preview.dart
@@ -1,6 +1,7 @@
 import 'package:Slydo/data/currency.dart';
 import 'package:Slydo/data/state_notifiers/user_bloc.dart';
 import 'package:Slydo/screens/more_apps/shopping/models/store.dart';
+import 'package:Slydo/screens/more_apps/shopping/screens/order/custom_pdf_print_order.dart';
 import 'package:Slydo/utils/colors.dart';
 import 'package:Slydo/utils/common.dart';
 import 'package:Slydo/utils/util.dart';
@@ -22,7 +23,7 @@ class OrderPreview extends StatefulWidget {
 class _OrderPreviewState extends State<OrderPreview> {
   Order? order;
   Product? product;
-
+  String? currency;
   @override
   void initState() {
     order = widget.arguments?['order'];
@@ -31,6 +32,7 @@ class _OrderPreviewState extends State<OrderPreview> {
 
   @override
   Widget build(BuildContext context) {
+    currency = worldCurrencies[order?.currency] ?? "";
     return Scaffold(
       appBar: _buildAppBar(),
       body: _buildBody(),
@@ -68,7 +70,14 @@ class _OrderPreviewState extends State<OrderPreview> {
         Padding(
           padding: const EdgeInsets.only(right: 15),
           child: GestureDetector(
-            onTap: () {},
+            onTap: () {
+              final CustomPdfPrintOrder pdfPrint = CustomPdfPrintOrder(
+                order: order,
+                currency: currency,
+                product: product,
+              );
+              pdfPrint.printOrderDetails();
+            },
             child: Image.asset(
               "assets/images/appIcon/printer.png",
               height: 22,
@@ -81,7 +90,6 @@ class _OrderPreviewState extends State<OrderPreview> {
   }
 
   Widget _buildBody() {
-    final currency = worldCurrencies[order?.currency] ?? "";
     return SingleChildScrollView(
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
       child: Column(
@@ -91,7 +99,7 @@ class _OrderPreviewState extends State<OrderPreview> {
           _buildHorizontalDotBorder(black),
           _buildOrderProductDetails(),
           _buildHorizontalDotBorder(lightBlackFont),
-          _buildProductPriceAndCharges(currency),
+          _buildProductPriceAndCharges(currency ?? ""),
           const SizedBox(height: 10.0),
           _buildDeliveryDetails(),
           const SizedBox(height: 20.0),
